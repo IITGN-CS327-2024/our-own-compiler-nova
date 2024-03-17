8da3bbeb297c7fe442495398c821a97452b98c63
Sahil <dassahil@iitgn.ac.in>
2024-03-18 05:03:58 +0530

    main function declared, create_graph func implemented, ast generated

--- src/parser_.py
+++ src/parser_.py
@@ -1,8 +1,159 @@
 import os
 import sys
+import def_node_classes, AST_transformer
+import rich
+
+from dataclasses import dataclass
+from lark import Lark, ast_utils, Transformer, v_args, Tree
+from lark.lexer import Lexer, Token
+from lark.tree import pydot__tree_to_png, Meta
 from lexer import lexer
-from lark import Lark
-from lark.tree import pydot__tree_to_png
+from typing import List
+from graphviz import Digraph
+
+
+this_module = sys.modules[__name__]
+
+# grammar = """
+#     start: statement*
+
+#     statement: variable_declaration_initialization 
+#             | function_call 
+#             | function_declaration 
+#             | conditional_statement
+#             | return_statement
+#             | loop_statement
+#             | loop_control_statement
+#             | array_declaration
+#             | tuple_declaration
+#             | array_operation
+#             | try_catch_statement
+#             | throw_statement
+#             | print_statement
+
+#     variable_declaration_initialization: variable_structure ";"
+
+#     variable_structure: "var" data_type identifier
+#                         | "var" data_type identifier "=" assigned_value
+#                         | identifier "=" assigned_value
+
+#     function_declaration: "fn" identifier "("parameter_list")" "::" data_type "{"statement*"}" ";"
+
+
+#     conditional_statement: "if" "("expression")" "{"statement*"}" ";"
+#                         | "if" "("expression")" "{"statement+"}" ";" "else" "{"statement*"}" ";"
+    
+#     return_statement: "return" expression ";"
+
+#     loop_statement: "loop" "(" expression ")" "{" statement* "}" ";"
+
+#     loop_control_statement: "break" ";" | "continue" ";"
+
+#     array_declaration: "var" "array" data_type identifier "=" "["expression_list"]" ";"
+#                     |   "var" "array" "("literal")" data_type identifier ";"
+    
+#     tuple_declaration: "var" "tuple" data_type identifier "=" "("expression_list")" ";"
+
+#     array_operation: identifier "." method_name
+
+#     method_name: "length" | "head" | "tail" | "cons" "("literal")" ";"
+
+#     try_catch_statement: "try" "{" statement* "}" ";" catch_block
+
+#     catch_block: "catch" "(" error_type ")" "{" statement* "}" ";" catch_block
+#                 | "catch" "(" "e" ")" "{" statement* "}" ";"
+
+#     error_type: "TypeError"
+#                 | "DivisionByZeroError" 
+#                 | "NameError" 
+#                 | "IndexError" 
+#                 | identifier
+
+#     throw_statement: "throw" error_type ";"
+
+#     print_statement: "println" "(" expression ")" ";"
+
+#     parameter_list: data_type identifier 
+#                     | data_type identifier "," parameter_list 
+#                     |
+    
+#     assigned_value: expression 
+#                   | function_call
+    
+#     function_call: identifier "(" expression_list ")" (";")?
+
+#     expression_list: expression 
+#                    | expression "," expression_list
+#                    |
+                     
+#     expression: condition
+#                 | identifier "["literal"]"
+#                 | identifier "["literal ":" literal"]"
+#                 | expression "|" condition
+#                 | function_call
+#                 | array_operation
+
+#     condition: condition "and" condition1 
+#                 | condition1
+#     condition1: condition1 "or" condition2 
+#                 | condition2
+#     condition2: "not" condition3 
+#                 | condition3
+#     condition3: arithmetic_operation 
+#                 | "-" arithmetic_operation 
+#                 | "!" arithmetic_operation 
+#                 | arithmetic_operation comparison_operation arithmetic_operation
+
+#     comparison_operation: ">" | "<" | ">=" | "<=" | "==" | "!="
+                
+#     arithmetic_operation: add_operand | array_operation
+#     add_operand: add_operand "+" mul_operand 
+#                 | add_operand "-" mul_operand 
+#                 | mul_operand
+
+#     mul_operand: mul_operand "*" terminal_operand 
+#                 | mul_operand "/" terminal_operand 
+#                 | terminal_operand
+
+#     terminal_operand: literal
+#                     | identifier
+#                     | "("expression")"
+
+#     literal: number
+#             | boolean 
+#             | string
+
+
+    
+#     number: NUMBER
+#     boolean: "true" | "false"
+#     string: /"[^"]*"/
+#     data_type: "int" | "bool" | "string" | "void"
+#     identifier: /[a-zA-Z][a-zA-Z0-9]*/
+    
+
+#     %import common.NUMBER
+#     %import common.WS
+#     %ignore WS
+# """
+
+# grammar = """
+#     start: statement*
+
+#     statement: variable_declaration_initialization
+
+#     variable_declaration_initialization: variable_structure END_OF_STMT
+
+#     variable_structure: KEYWORD KEYWORD IDENTIFIER
+
+#     KEYWORD: "var" | "int" | "bool" | "string" | "void"
+#     IDENTIFIER: /[a-zA-Z][a-zA-Z0-9]*/
+#     END_OF_STMT: ";"
+# """
+
+
+
+
 
 grammar = """
     start: statement*
@@ -21,240 +172,190 @@ grammar = """
             | throw_statement
             | print_statement
 
-    variable_declaration_initialization: variable_structure ";"
+    variable_declaration_initialization:  KEYWORD KEYWORD IDENTIFIER END_OF_STMT
+                                        | KEYWORD KEYWORD IDENTIFIER ASSIGN assigned_value END_OF_STMT
+                                        | IDENTIFIER ASSIGN assigned_value END_OF_STMT
 
-    variable_structure: "var" data_type identifier
-                        | "var" data_type identifier "=" assigned_value
-                        | identifier "=" assigned_value
-
-    function_declaration: "fn" identifier "("parameter_list")" "::" data_type "{"statement*"}" ";"
+    function_declaration: KEYWORD IDENTIFIER LEFT_PARENTHESIS parameter_list RIGHT_PARENTHESIS DOUBLE_COLON KEYWORD LEFT_BRACES statement* RIGHT_BRACES END_OF_STMT
 
 
-    conditional_statement: "if" "("expression")" "{"statement*"}" ";"
-                        | "if" "("expression")" "{"statement+"}" ";" "else" "{"statement*"}" ";"
+    conditional_statement: KEYWORD LEFT_PARENTHESIS expression RIGHT_PARENTHESIS LEFT_BRACES statement* RIGHT_BRACES END_OF_STMT
+                        | KEYWORD LEFT_PARENTHESIS expression RIGHT_PARENTHESIS LEFT_BRACES statement+ RIGHT_BRACES END_OF_STMT KEYWORD LEFT_BRACES statement* RIGHT_BRACES END_OF_STMT
     
-    return_statement: "return" expression ";"
+    return_statement: KEYWORD expression END_OF_STMT
 
-    loop_statement: "loop" "(" expression ")" "{" statement* "}" ";"
+    loop_statement: KEYWORD LEFT_PARENTHESIS  expression  RIGHT_PARENTHESIS LEFT_BRACES  statement*  RIGHT_BRACES END_OF_STMT
 
-    loop_control_statement: "break" ";" | "continue" ";"
+    loop_control_statement: KEYWORD END_OF_STMT | KEYWORD END_OF_STMT
 
-    array_declaration: "var" "array" data_type identifier "=" "["expression_list"]" ";"
-                    |   "var" "array" "("literal")" data_type identifier ";"
+    array_declaration: KEYWORD KEYWORD KEYWORD IDENTIFIER ASSIGN LEFT_BRACKET expression_list RIGHT_BRACKET END_OF_STMT
+                    |   KEYWORD KEYWORD LEFT_PARENTHESIS literal RIGHT_PARENTHESIS KEYWORD IDENTIFIER END_OF_STMT
     
-    tuple_declaration: "var" "tuple" data_type identifier "=" "("expression_list")" ";"
+    tuple_declaration: KEYWORD KEYWORD KEYWORD IDENTIFIER ASSIGN LEFT_PARENTHESIS expression_list RIGHT_PARENTHESIS END_OF_STMT
 
-    array_operation: identifier "." method_name
+    array_operation: IDENTIFIER ARRAY_OPERATOR ARRAY_OPERATION
+                    | IDENTIFIER ARRAY_OPERATOR ARRAY_OPERATION LEFT_PARENTHESIS literal RIGHT_PARENTHESIS END_OF_STMT
 
-    method_name: "length" | "head" | "tail" | "cons" "("literal")" ";"
+    ARRAY_OPERATION: "length" | "head" | "tail" | "cons"
 
-    try_catch_statement: "try" "{" statement* "}" ";" catch_block
+    try_catch_statement: KEYWORD LEFT_BRACES  statement*  RIGHT_BRACES END_OF_STMT catch_block
 
-    catch_block: "catch" "(" error_type ")" "{" statement* "}" ";" catch_block
-                | "catch" "(" "e" ")" "{" statement* "}" ";"
+    catch_block: KEYWORD LEFT_PARENTHESIS  ERROR_TYPE  RIGHT_PARENTHESIS LEFT_BRACES  statement*  RIGHT_BRACES END_OF_STMT catch_block
+                |
 
-    error_type: "TypeError"
+    ERROR_TYPE: "TypeError"
                 | "DivisionByZeroError" 
                 | "NameError" 
                 | "IndexError" 
-                | identifier
+                | "e"
 
-    throw_statement: "throw" error_type ";"
+    throw_statement: KEYWORD ERROR_TYPE END_OF_STMT
 
-    print_statement: "println" "(" expression ")" ";"
+    print_statement: KEYWORD LEFT_PARENTHESIS expression RIGHT_PARENTHESIS END_OF_STMT
 
-    parameter_list: data_type identifier 
-                    | data_type identifier "," parameter_list 
+    parameter_list: KEYWORD IDENTIFIER 
+                    | KEYWORD IDENTIFIER SEPERATOR parameter_list 
                     |
     
     assigned_value: expression 
                   | function_call
     
-    function_call: identifier "(" expression_list ")" (";")?
+    function_call: IDENTIFIER LEFT_PARENTHESIS  expression_list  RIGHT_PARENTHESIS
+                 | IDENTIFIER LEFT_PARENTHESIS  expression_list  RIGHT_PARENTHESIS END_OF_STMT
 
     expression_list: expression 
-                   | expression "," expression_list
+                   | expression SEPERATOR expression_list
                    |
                      
     expression: condition
-                | identifier "["literal"]"
-                | identifier "["literal ":" literal"]"
-                | expression "|" condition
+                | IDENTIFIER LEFT_BRACKET literal RIGHT_BRACKET
+                | IDENTIFIER LEFT_BRACKET literal SEPERATOR literal RIGHT_BRACKET
+                | expression STRING_OPERATOR condition
                 | function_call
                 | array_operation
 
-    condition: condition "and" condition1 
+    condition: condition LOGICAL_OPERATOR condition1 
                 | condition1
-    condition1: condition1 "or" condition2 
+    condition1: condition1 LOGICAL_OPERATOR condition2 
                 | condition2
-    condition2: "not" condition3 
+    condition2: LOGICAL_OPERATOR condition3 
                 | condition3
     condition3: arithmetic_operation 
-                | "-" arithmetic_operation 
-                | "!" arithmetic_operation 
-                | arithmetic_operation comparison_operation arithmetic_operation
+                | ARITHMETIC_OPERATOR arithmetic_operation 
+                | UNARY_NOT arithmetic_operation 
+                | arithmetic_operation COMPARISON_OPERATOR arithmetic_operation
 
-    comparison_operation: ">" | "<" | ">=" | "<=" | "==" | "!="
+    COMPARISON_OPERATOR: ">" | "<" | ">=" | "<=" | "==" | "!="
                 
     arithmetic_operation: add_operand | array_operation
-    add_operand: add_operand "+" mul_operand 
-                | add_operand "-" mul_operand 
+    add_operand: add_operand ARITHMETIC_OPERATOR mul_operand 
+                | add_operand ARITHMETIC_OPERATOR mul_operand 
                 | mul_operand
 
-    mul_operand: mul_operand "*" terminal_operand 
-                | mul_operand "/" terminal_operand 
+    mul_operand: mul_operand ARITHMETIC_OPERATOR terminal_operand 
+                | mul_operand ARITHMETIC_OPERATOR terminal_operand 
                 | terminal_operand
 
     terminal_operand: literal
-                    | identifier
-                    | "("expression")"
+                    | IDENTIFIER
+                    | LEFT_PARENTHESIS expression RIGHT_PARENTHESIS
 
-    literal: number
-            | boolean 
-            | string
+    literal: NUMBER
+            | BOOLEAN 
+            | STRING
 
 
-    
-    number: NUMBER
-    boolean: "true" | "false"
-    string: /"[^"]*"/
-    data_type: "int" | "bool" | "string" | "void"
-    identifier: /[a-zA-Z][a-zA-Z0-9]*/
-    
+    NUMBER: /[1-9][0-9]*/
+    BOOLEAN: "true" | "false"
+    STRING: /"[^"]*"/
+    IDENTIFIER: /[a-zA-Z][a-zA-Z0-9]*/
+    KEYWORD: "var" | "int" | "bool" | "string" | "println" | "array" | "tuple" | "if" | "else" | "loop" | "fn" | "return" | "void" | "break" | "continue" | "try" | "catch" | "throw"
 
-    %import common.NUMBER
-    %import common.WS
-    %ignore WS
+    ARITHMETIC_OPERATOR: "+" | "-" | "*" | "/"
+    COMPARE_OPERATOR: ">" | "<" | ">=" | "<=" | "==" | "!="
+    LOGICAL_OPERATOR: "and" | "or" | "not"
+
+    LEFT_PARENTHESIS: "("
+    RIGHT_PARENTHESIS: ")"
+    LEFT_BRACES: "{"
+    RIGHT_BRACES: "}"
+    LEFT_BRACKET: "["
+    RIGHT_BRACKET: "]"
+
+    DOUBLE_COLON: "::"
+    SEPERATOR: "," | ":"
+    ASSIGN: "="
+    ARRAY_OPERATOR: "."
+    UNARY_NOT: "!"
+    STRING_OPERATOR: "|"
+    END_OF_STMT: ";"
 """
 
+tokens = []
+# Custom lexer class that creates a generator object for each tokens
+class TypeLexer(Lexer):
+    def __init__(self, lexer_conf):
+        pass
 
-def parser(tokens: list, file_name: str):
+    def lex(self, data):
+        for i in range(len(tokens)):
+            token_class = tokens[i][0]
+            token_value = tokens[i][1]
+
+            yield Token(token_class, token_value)
+
+# Returns the tokens generated by the lexer
+def generate_tokens(tokens: list, file_name: str):
     file_path = os.path.join("..", "testcases", f"{file_name}")
     with open(file_path, "r") as code:
         for line in code:
             lexer(line, tokens)
+    # print(tokens)
     return tokens
 
+def create_graph(tree, graph=None):
+    if graph is None:
+        graph = Digraph()
 
-if len(sys.argv) != 2:
-    print(f"Expected 2 argments given {len(sys.argv)} arguments") 
-    sys.exit(1)
-file_name = sys.argv[1]
+    if isinstance(tree, def_node_classes.ASTNode):
+        children = vars(tree).items()
+        for _,child in children:
+            if isinstance(child, def_node_classes.ASTNode):
+                graph.node(str(id(child)), label = str(child), filled='true')
+                graph.edge(str(id(tree)), str(id(child)))
+                create_graph(child, graph)
 
-tokens = []
-tokens = parser(tokens, file_name)
-# print(tokens)
-
-
-
-parser_ = Lark(grammar)
-
-input_text = ""
-for i in range(len(tokens)):
-    input_text += tokens[i][1]
-    if i != len(tokens) - 1:
-        input_text += " "
-
-def error_handler(err):
-    print(err)
-
-parse_tree = parser_.parse(input_text)
-print(parse_tree)
-print(parse_tree.pretty())
-# print(pydot__tree_to_png(parse_tree, "parse_tree.png"))
+            else:
+                graph.node(str(id(child)), label=str(child), shape='box', filled='true')
+                graph.edge(str(id(tree)), str(id(child)))
+    return graph
 
 
 
+if __name__ == "__main__":
+    # Checking inputs from the user and passing it to the parser
+    if len(sys.argv) != 2:
+        print(f"Expected 2 argments given {len(sys.argv)} arguments") 
+        sys.exit(1)
+    file_name = sys.argv[1]
 
+    # Generating the tokens using lexer and setting up the parser to use custom lexer's output
+    tokens = generate_tokens(tokens, file_name)
+    parser = Lark(grammar, strict=True, lexer=TypeLexer)
 
+    with open(f"../testcases/{file_name}", "r") as code:
+        source_code = code.read()
 
+    # Generating the parse tree
+    parse_tree = parser.parse(source_code)
+    print(parse_tree)
+    print(parse_tree.pretty())
+    pydot__tree_to_png(parse_tree, "parse_tree.png")
+    print("Parse tree generated successfully, check parse_tree.png file for the parse tree.")
 
+    # Building the AST
+    transformer = AST_transformer.CustomTransformer()
+    ast = transformer.transform(parse_tree)
 
-
-
-
-
-
-
-
-
-
-
-
-# import os
-# import sys
-# from lexer import lexer
-# from lark import Lark
-# from lark.tree import pydot__tree_to_png
-
-# grammar = """
-#     start: statement*
-
-#     statement: variable_declaration | array_declaration | tuple_declaration | if_statement | loop_statement | print_statement | function_declaration | try_catch_statement | function_call
-
-#     variable_declaration: "var" type identifier "=" expression ";" 
-#     array_declaration: "var" "array" type identifier "=" "[" expression_list "]" ";"
-#     tuple_declaration: "var" "tuple" type identifier "=" "(" expression_list ")" ";"
-#     if_statement: "if" "(" expression ")" "{" statement* "}" ("else" "{" statement* "}")?
-#     loop_statement: "loop" "(" expression ")" "{" statement* "}" 
-#     print_statement: "println" "(" expression ")" ";" 
-#     function_declaration: "fn" identifier "(" parameter_list ")" "::" type "{" statement* "}" ";"
-#     try_catch_statement: "try" "{" statement* "}" catch_block+ 
-#     catch_block: "catch" "(" identifier ")" "{" statement* "}"
-#     function_call: identifier "(" expression_list ")" ";"
-
-#     type: "int" | "bool" | "string" | "void"
-#     expression_list: expression ("," expression)*
-#     expression: literal | identifier | unary_expression | binary_expression | array_access | function_call | "(" expression ")"
-#     literal: number | boolean | string
-#     number: NUMBER
-#     boolean: "true" | "false"
-#     string: /"[^"]*"/
-#     identifier: /[a-zA-Z_]\w*/
-#     unary_expression: ("-" | "!") expression
-#     binary_expression: expression ("+" | "-" | "*" | "/" | "and" | "or" | ">" | "<" | ">=" | "<=" | "==" | "!=") expression
-#     array_access: identifier "[" expression "]" | identifier "[" NUMBER ":" NUMBER "]"
-#     parameter_list: (type identifier ("," type identifier)*)?
-
-#     %import common.NUMBER
-#     %import common.WS
-#     %ignore WS
-
-    
-# """
-
-
-# def parser(tokens: list, file_name: str):
-#     file_path = os.path.join("..", "testcases", f"{file_name}")
-#     with open(file_path, "r") as code:
-#         for line in code:
-#             lexer(line, tokens)
-#     return tokens
-
-
-# if len(sys.argv) != 2:
-#     print(f"Expected 2 argments given {len(sys.argv)} arguments") 
-#     sys.exit(1)
-# file_name = sys.argv[1]
-
-# tokens = []
-# tokens = parser(tokens, file_name)
-# # print(tokens)
-
-
-
-# parser_ = Lark(grammar)
-
-# input_text = ""
-# # input_text = " var int a = 5; var int b = 7;"
-# for i in range(len(tokens)):
-#     input_text += tokens[i][1]
-#     if i != len(tokens) - 1:
-#         input_text += " "
-
-# # print(input_text)
-
-# parse_tree = parser_.parse(input_text)
-# print(parse_tree)
-# print(parse_tree.pretty())
-# # print(pydot__tree_to_png(parse_tree, "parse_tree.png"))
+    graph = create_graph(ast)
+    graph.render('AST', format='png', view=True)
